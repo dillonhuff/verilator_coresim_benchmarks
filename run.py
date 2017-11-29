d8592e22e16dcdb98d8d95cd18eccd9f38a8f8e3
Dillon Huff <dillonhuff@gmail.com>
2017-11-29 11:39:24 -0800

    Now have timing printouts

--- run.py
+++ run.py
@@ -8,6 +8,28 @@ os.system("mkdir scratch");
 call(["make", "-C" , "./conv_3_1/coresim/"])
 os.system("./conv_3_1/coresim/a.out > scratch/coresim_conv_3_1_out.txt");
 
+coresim_result = file("scratch/coresim_conv_3_1_out.txt", "r");
+
+coresim_result_text = coresim_result.read()
+coresim_result_lines = coresim_result_text.splitlines()
+num_lines = len(coresim_result_lines)
+
+result_lines = coresim_result_lines[num_lines - 3:num_lines - 1]
+print 'CoreSIM: ', result_lines[0]
+print 'CoreSIM: ', result_lines[1]
+
+#print coresim_result_text
+
 os.system("make  -C ./conv_3_1/verilator/ > scratch/verilator_conv_3_1_out.txt");
+
+verilator_result = file("scratch/verilator_conv_3_1_out.txt", "r");
+
+verilator_result_text = verilator_result.read()
+verilator_result_lines = verilator_result_text.splitlines()
+num_lines = len(verilator_result_lines)
+
+result_lines = verilator_result_lines[num_lines - 3:num_lines - 1]
+print 'Verilator: ', result_lines[0]
+print 'Verilator: ', result_lines[1]
 
 # Harris
