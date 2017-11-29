ddc0694a3e9b641ee367ddb5ca2a157f68676dbd
Dillon Huff <dillonhuff@gmail.com>
2017-11-29 13:47:43 -0800

    Created benchmark_application function to wrap boilerplate build code

--- run.py
+++ run.py
@@ -1,59 +1,41 @@
 from subprocess import call
 import os
 
+# Utility functions
+def benchmark_application(app_name):
+    call(["make", "-C" , "./" + app_name + "/coresim/"])
+    os.system("./" + app_name + "/coresim/a.out > scratch/coresim_" + app_name + "_out.txt");
+
+    coresim_result = file("scratch/coresim_" + app_name + "_out.txt", "r");
+
+    coresim_result_text = coresim_result.read()
+    coresim_result_lines = coresim_result_text.splitlines()
+    num_lines = len(coresim_result_lines)
+
+    result_lines = coresim_result_lines[num_lines - 3:num_lines - 1]
+    print 'CoreSIM  : ', result_lines[0]
+    print 'CoreSIM  : ', result_lines[1]
+
+    os.system("make  -C ./" + app_name + "/verilator/ > scratch/verilator_" + app_name + "_out.txt");
+
+    verilator_result = file("scratch/verilator_" + app_name + "_out.txt", "r");
+
+    verilator_result_text = verilator_result.read()
+    verilator_result_lines = verilator_result_text.splitlines()
+    num_lines = len(verilator_result_lines)
+
+    result_lines = verilator_result_lines[num_lines - 3:num_lines - 1]
+    print 'Verilator: ', result_lines[0]
+    print 'Verilator: ', result_lines[1]
+    
+
 #Setup
 os.system("mkdir scratch");
 
 # conv_3_1
-call(["make", "-C" , "./conv_3_1/coresim/"])
-os.system("./conv_3_1/coresim/a.out > scratch/coresim_conv_3_1_out.txt");
-
-coresim_result = file("scratch/coresim_conv_3_1_out.txt", "r");
-
-coresim_result_text = coresim_result.read()
-coresim_result_lines = coresim_result_text.splitlines()
-num_lines = len(coresim_result_lines)
-
-result_lines = coresim_result_lines[num_lines - 3:num_lines - 1]
-print 'CoreSIM  : ', result_lines[0]
-print 'CoreSIM  : ', result_lines[1]
-
-os.system("make  -C ./conv_3_1/verilator/ > scratch/verilator_conv_3_1_out.txt");
-
-verilator_result = file("scratch/verilator_conv_3_1_out.txt", "r");
-
-verilator_result_text = verilator_result.read()
-verilator_result_lines = verilator_result_text.splitlines()
-num_lines = len(verilator_result_lines)
-
-result_lines = verilator_result_lines[num_lines - 3:num_lines - 1]
-print 'Verilator: ', result_lines[0]
-print 'Verilator: ', result_lines[1]
+benchmark_application("conv_3_1")
 
 # Harris
 
 # Parallel logic ops stress test
-call(["make", "-C" , "./parallel_ops/coresim/"])
-os.system("./parallel_ops/coresim/a.out > scratch/coresim_parallel_ops_out.txt");
-
-coresim_result = file("scratch/coresim_parallel_ops_out.txt", "r");
-
-coresim_result_text = coresim_result.read()
-coresim_result_lines = coresim_result_text.splitlines()
-num_lines = len(coresim_result_lines)
-
-result_lines = coresim_result_lines[num_lines - 3:num_lines - 1]
-print 'CoreSIM  : ', result_lines[0]
-print 'CoreSIM  : ', result_lines[1]
-
-os.system("make  -C ./parallel_ops/verilator/ > scratch/verilator_parallel_ops_out.txt");
-
-verilator_result = file("scratch/verilator_parallel_ops_out.txt", "r");
-
-verilator_result_text = verilator_result.read()
-verilator_result_lines = verilator_result_text.splitlines()
-num_lines = len(verilator_result_lines)
-
-result_lines = verilator_result_lines[num_lines - 3:num_lines - 1]
-print 'Verilator: ', result_lines[0]
-print 'Verilator: ', result_lines[1]
+benchmark_application("parallel_ops")
