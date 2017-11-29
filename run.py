a04bce5cdecc4757033cc58244fcf3aa9fe61957
Dillon Huff <dillonhuff@gmail.com>
2017-11-29 11:43:23 -0800

    Minor changes to script

--- run.py
+++ run.py
@@ -15,8 +15,8 @@ coresim_result_lines = coresim_result_text.splitlines()
 num_lines = len(coresim_result_lines)
 
 result_lines = coresim_result_lines[num_lines - 3:num_lines - 1]
-print 'CoreSIM: ', result_lines[0]
-print 'CoreSIM: ', result_lines[1]
+print 'CoreSIM  : ', result_lines[0]
+print 'CoreSIM  : ', result_lines[1]
 
 #print coresim_result_text
 
@@ -33,3 +33,5 @@ print 'Verilator: ', result_lines[0]
 print 'Verilator: ', result_lines[1]
 
 # Harris
+
+# Parallel logic ops stress test
