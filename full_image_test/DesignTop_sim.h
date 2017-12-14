d42aa3d6dde100df5e53e2e9f7a0dd14a886240c
Dillon Huff <dillonhuff@gmail.com>
2017-12-13 22:48:44 -0800

    Now working on creating a real comparison test suite

---
+++ full_image_test/DesignTop_sim.h
@@ -0,0 +1,21 @@
+#include <stdint.h>
+#include <cstdio>
+
+#include "bit_vector.h"
+
+using namespace bsim;
+
+struct __attribute__((packed, aligned(32))) circuit_state {
+	uint8_t self_clk_last;
+	uint8_t self_clk;
+	uint8_t lb_p4_clamped_stencil_update_stream$mem_2$waddr$reg0;
+	uint8_t lb_p4_clamped_stencil_update_stream$mem_2$raddr$reg0;
+	uint16_t lb_p4_clamped_stencil_update_stream$mem_2$mem[ 10 ];
+	uint8_t lb_p4_clamped_stencil_update_stream$mem_1$waddr$reg0;
+	uint8_t lb_p4_clamped_stencil_update_stream$mem_1$raddr$reg0;
+	uint16_t lb_p4_clamped_stencil_update_stream$mem_1$mem[ 10 ];
+	uint16_t self_out;
+	uint16_t self_in_0;
+};
+
+void simulate( circuit_state* state );
---
+++ full_image_test/main.cpp
@@ -0,0 +1,65 @@
+#include "DesignTop_sim.h"
+
+#include "lodepng.h"
+
+#include <bitset>
+
+using namespace std;
+
+int main() {
+  std::vector<unsigned char> image; //the raw pixels
+  unsigned width, height;
+
+  unsigned error = lodepng::decode(image, width, height, "./input_conv_bw.png");
+  //unsigned error = lodepng::decode(image, width, height, "./input.png");
+
+  //if there's an error, display it
+  if(error) {
+    std::cout << "decoder error " << error << ": " << lodepng_error_text(error) << std::endl;
+  }
+
+  cout << "Width  = " << width << endl;
+  cout << "Height = " << height << endl;
+
+  cout << "Starting CoreSim" << endl;
+  circuit_state state;
+
+  // Set defaults
+  for (int i = 0; i < 10; i++) {
+    state.lb_p4_clamped_stencil_update_stream$mem_1$mem[ i ]= 0;
+  }
+
+  state.lb_p4_clamped_stencil_update_stream$mem_1$raddr$reg0 = 0;
+  state.lb_p4_clamped_stencil_update_stream$mem_1$waddr$reg0 = 0;
+
+  for (int i = 0; i < 10; i++) {
+    state.lb_p4_clamped_stencil_update_stream$mem_2$mem[ i ] = 0;
+  }
+
+  state.lb_p4_clamped_stencil_update_stream$mem_2$raddr$reg0 = 0;
+  state.lb_p4_clamped_stencil_update_stream$mem_2$waddr$reg0 = 0;
+
+  state.self_in_0 = 1;
+  state.self_clk = 1;
+  state.self_clk_last = 0;
+
+  state.self_clk_last = 1;
+  for (int i = 0; i < image.size(); i++) {
+    state.self_clk = i % 2;
+
+    simulate(&state);
+
+    state.self_clk_last = state.self_clk;
+    
+    if ((state.self_clk_last == 0) &&
+        ((i + 1) % 2 == 1)) {
+      cout << "out " << i << " = " << state.self_out << endl;
+    }
+
+    if ((state.self_clk_last == 1) &&
+        ((i + 1) % 2 == 0)) {
+      state.self_in_0 = image[i];
+    }
+  }
+  
+}
