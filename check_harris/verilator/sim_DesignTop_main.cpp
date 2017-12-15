520a7b502b59556697f3f46769d384968ffc8225
Dillon Huff <dillonhuff@gmail.com>
2017-12-15 12:38:35 -0800

    Verilator and CoreSIM disagree on first cycle value even with all state set zeroed. Not sure if this is a combinational logic difference or what

--- check_harris/verilator/sim_DesignTop_main.cpp
+++ check_harris/verilator/sim_DesignTop_main.cpp
@@ -13,6 +13,12 @@
 
 using namespace std;
 
+void set_memory(uint16_t* const mem, const int len) {
+  for (int i = 0; i < len; i++) {
+    mem[i] = 0;
+  }
+}
+
 void set_defaults(circuit_state* state) {
   state->in_0_auto_reg = 0;
   state->self_clk_last = 0;
@@ -24,39 +30,69 @@ void set_defaults(circuit_state* state) {
   state->lb_padded_2_stencil_update_stream$reg_2_1 = 0;
   state->lb_padded_2_stencil_update_stream$mem_2$waddr$reg0 = 0;
   state->lb_padded_2_stencil_update_stream$mem_2$raddr$reg0 = 0;
-  //state->lb_padded_2_stencil_update_stream$mem_2$mem[ 486 ];
+
+  //state->lb_padded_2_stencil_update_stream$mem_2$mem[ 486 ];  
+  set_memory( state->lb_padded_2_stencil_update_stream$mem_2$mem, 486 );
+
   state->lb_padded_2_stencil_update_stream$mem_1$waddr$reg0 = 0;
   state->lb_padded_2_stencil_update_stream$mem_1$raddr$reg0 = 0;
+
   //state->lb_padded_2_stencil_update_stream$mem_1$mem[ 486 ];
+  set_memory(state->lb_padded_2_stencil_update_stream$mem_1$mem, 486);
+
   state->lb_p3_cim_stencil_update_stream$reg_1_2 = 0;
   state->lb_p3_cim_stencil_update_stream$reg_0_2 = 0;
   state->lb_p3_cim_stencil_update_stream$reg_0_1 = 0;
   state->lb_p3_cim_stencil_update_stream$reg_2_1 = 0;
   state->lb_p3_cim_stencil_update_stream$mem_2$waddr$reg0 = 0;
   state->lb_p3_cim_stencil_update_stream$mem_2$raddr$reg0 = 0;
+
   //state->lb_p3_cim_stencil_update_stream$mem_2$mem[ 482 ];
+  set_memory( state->lb_p3_cim_stencil_update_stream$mem_2$mem, 482 );
+
   state->lb_p3_cim_stencil_update_stream$reg_1_1 = 0;
   state->lb_p3_cim_stencil_update_stream$mem_1$waddr$reg0 = 0;
   state->lb_p3_cim_stencil_update_stream$mem_1$raddr$reg0 = 0;
+
   //state->lb_p3_cim_stencil_update_stream$mem_1$mem[ 482 ];
+  set_memory(state->lb_p3_cim_stencil_update_stream$mem_1$mem, 482 );
+
   state->lb_grad_yy_2_stencil_update_stream$mem_2$waddr$reg0 = 0;
   state->lb_grad_yy_2_stencil_update_stream$mem_2$raddr$reg0 = 0;
+
   //state->lb_grad_yy_2_stencil_update_stream$mem_2$mem[ 484 ];
+  set_memory(state->lb_grad_yy_2_stencil_update_stream$mem_2$mem, 484 );
+
   state->lb_grad_yy_2_stencil_update_stream$mem_1$waddr$reg0 = 0;
   state->lb_grad_yy_2_stencil_update_stream$mem_1$raddr$reg0 = 0;
+
   //state->lb_grad_yy_2_stencil_update_stream$mem_1$mem[ 484 ];
+  set_memory(state->lb_grad_yy_2_stencil_update_stream$mem_1$mem, 484 );
+
   state->lb_grad_xy_2_stencil_update_stream$mem_2$waddr$reg0 = 0;
   state->lb_grad_xy_2_stencil_update_stream$mem_2$raddr$reg0 = 0;
+
   //state->lb_grad_xy_2_stencil_update_stream$mem_2$mem[ 484 ];
+  set_memory(state->lb_grad_xy_2_stencil_update_stream$mem_2$mem, 484);
+
   state->lb_grad_xy_2_stencil_update_stream$mem_1$waddr$reg0 = 0;
   state->lb_grad_xy_2_stencil_update_stream$mem_1$raddr$reg0 = 0;
+
   //state->lb_grad_xy_2_stencil_update_stream$mem_1$mem[ 484 ];
+  set_memory(state->lb_grad_xy_2_stencil_update_stream$mem_1$mem, 484);
+
   state->lb_grad_xx_2_stencil_update_stream$mem_2$waddr$reg0 = 0;
   state->lb_grad_xx_2_stencil_update_stream$mem_2$raddr$reg0 = 0;
+
   //state->lb_grad_xx_2_stencil_update_stream$mem_2$mem[ 484 ];
+  set_memory(state->lb_grad_xx_2_stencil_update_stream$mem_2$mem, 484);
+
   state->lb_grad_xx_2_stencil_update_stream$mem_1$waddr$reg0 = 0;
   state->lb_grad_xx_2_stencil_update_stream$mem_1$raddr$reg0 = 0;
+
   //state->lb_grad_xx_2_stencil_update_stream$mem_1$mem[ 484 ];
+  set_memory(state->lb_grad_xx_2_stencil_update_stream$mem_1$mem, 484);
+
   state->lb_grad_yy_2_stencil_update_stream$reg_0_1 = 0;
   state->lb_grad_yy_2_stencil_update_stream$reg_0_2 = 0;
   state->lb_grad_yy_2_stencil_update_stream$reg_1_1 = 0;
@@ -79,7 +115,71 @@ void set_defaults(circuit_state* state) {
   //state->lb_grad_yy_2_stencil_update_stream$reg_2_2 = 0;
   //state->lb_p3_cim_stencil_update_stream$reg_2_2 = 0;
   state->lb_padded_2_stencil_update_stream$reg_1_1 = 0;
-  
+
+
+  // Defaults again
+  state->self_clk_last= 0;
+  state->self_clk= 0;
+  state->lb_padded_2_stencil_update_stream$reg_2_2= 0;
+  state->lb_padded_2_stencil_update_stream$reg_1_2= 0;
+  state->lb_padded_2_stencil_update_stream$reg_0_2= 0;
+  state->lb_padded_2_stencil_update_stream$reg_2_1= 0;
+  state->lb_padded_2_stencil_update_stream$mem_2$waddr$reg0= 0;
+  state->lb_padded_2_stencil_update_stream$mem_2$raddr$reg0= 0;
+
+  state->lb_padded_2_stencil_update_stream$mem_1$waddr$reg0= 0;
+  state->lb_padded_2_stencil_update_stream$mem_1$raddr$reg0= 0;
+
+  state->lb_p3_cim_stencil_update_stream$reg_1_2= 0;
+  state->lb_p3_cim_stencil_update_stream$reg_0_2= 0;
+  state->lb_p3_cim_stencil_update_stream$reg_0_1= 0;
+  state->lb_p3_cim_stencil_update_stream$reg_2_1= 0;
+  state->lb_p3_cim_stencil_update_stream$mem_2$waddr$reg0= 0;
+  state->lb_p3_cim_stencil_update_stream$mem_2$raddr$reg0= 0;
+
+  state->lb_p3_cim_stencil_update_stream$reg_1_1= 0;
+  state->lb_p3_cim_stencil_update_stream$mem_1$waddr$reg0= 0;
+  state->lb_p3_cim_stencil_update_stream$mem_1$raddr$reg0= 0;
+
+  state->lb_grad_yy_2_stencil_update_stream$mem_2$waddr$reg0= 0;
+  state->lb_grad_yy_2_stencil_update_stream$mem_2$raddr$reg0= 0;
+
+  state->lb_grad_yy_2_stencil_update_stream$mem_1$waddr$reg0= 0;
+  state->lb_grad_yy_2_stencil_update_stream$mem_1$raddr$reg0= 0;
+
+  state->lb_grad_xy_2_stencil_update_stream$mem_2$waddr$reg0= 0;
+  state->lb_grad_xy_2_stencil_update_stream$mem_2$raddr$reg0= 0;
+
+  state->lb_grad_xy_2_stencil_update_stream$mem_1$waddr$reg0= 0;
+  state->lb_grad_xy_2_stencil_update_stream$mem_1$raddr$reg0= 0;
+
+  state->lb_grad_xx_2_stencil_update_stream$mem_2$waddr$reg0= 0;
+  state->lb_grad_xx_2_stencil_update_stream$mem_2$raddr$reg0= 0;
+
+  state->lb_grad_xx_2_stencil_update_stream$mem_1$waddr$reg0= 0;
+  state->lb_grad_xx_2_stencil_update_stream$mem_1$raddr$reg0= 0;
+
+  state->lb_padded_2_stencil_update_stream$reg_0_1= 0;
+  state->lb_grad_yy_2_stencil_update_stream$reg_0_1= 0;
+  state->lb_grad_yy_2_stencil_update_stream$reg_0_2= 0;
+  state->lb_grad_yy_2_stencil_update_stream$reg_1_1= 0;
+  state->lb_grad_yy_2_stencil_update_stream$reg_1_2= 0;
+  state->lb_grad_yy_2_stencil_update_stream$reg_2_1= 0;
+  state->lb_grad_xy_2_stencil_update_stream$reg_0_1= 0;
+  state->lb_grad_xy_2_stencil_update_stream$reg_0_2= 0;
+  state->lb_grad_xy_2_stencil_update_stream$reg_1_1= 0;
+  state->lb_grad_xy_2_stencil_update_stream$reg_1_2= 0;
+  state->lb_grad_xy_2_stencil_update_stream$reg_2_1= 0;
+  state->lb_grad_xx_2_stencil_update_stream$reg_0_1= 0;
+  state->lb_grad_xx_2_stencil_update_stream$reg_0_2= 0;
+  state->lb_grad_xx_2_stencil_update_stream$reg_1_1= 0;
+  state->lb_grad_xx_2_stencil_update_stream$reg_1_2= 0;
+  state->lb_grad_xx_2_stencil_update_stream$reg_2_1= 0;
+  state->in_0_auto_reg= 0;
+  state->self_out= 0;
+  state->lb_padded_2_stencil_update_stream$reg_1_1= 0;
+  state->self_in_0= 0;
+
 }
 
 
@@ -112,13 +212,13 @@ int main(int argc, char** argv) {
   top->in_0 = 1;//in_0;
   top->out = 0;//out;
 
-  // circuit_state state;
-  // set_defaults(&state);
+  circuit_state state;
+  set_defaults(&state);
 
-  // state.self_clk = 1;
-  // state.self_clk_last = 0;
-  // state.self_in_0 = 1;
-  // state.self_out = 0;
+  state.self_clk = 1;
+  state.self_clk_last = 0;
+  state.self_in_0 = 1;
+  state.self_out = 0;
   
   vector<unsigned char> output;
 
@@ -129,13 +229,13 @@ int main(int argc, char** argv) {
   for (int i = 0; i < image.size(); i++) {
     top->clk = i % 2;
 
-    //state.self_clk = i % 2;
+    state.self_clk = i % 2;
 
     top->eval();
 
-    // simulate(&state);
+    simulate(&state);
 
-    // state.self_clk_last = state.self_clk;
+    state.self_clk_last = state.self_clk;
 
 
     // cout << (int) top->v__DOT__smax_762_763_764__DOT__max_mux___05Fout << endl;
@@ -148,20 +248,20 @@ int main(int argc, char** argv) {
     //     	    & (0x64U <= (IData)(vlTOPp->v__DOT__lb_p3_cim_stencil_update_stream__DOT__reg_1_1__DOT__outReg)))
     //     	    ? 0U : 0xffU);
     
-    //cout << "out = " << (int) top->out << endl;
+    // cout << "out = " << (int) top->out << endl;
 
-    // cout << "--- Half cycle" << endl;
-    // cout << "top->in_0       = " << (int) top->in_0 << endl;
-    // cout << "state.self_in_0 = " << (int) state.self_in_0 << endl;
+    cout << "--- Half cycle" << endl;
+    cout << "top->in_0       = " << (int) top->in_0 << endl;
+    cout << "state.self_in_0 = " << (int) state.self_in_0 << endl;
 
-    // assert(top->in_0 == state.self_in_0);
-    // uint16_t rv = state.lb_padded_2_stencil_update_stream$reg_0_1;
-    // uint16_t eval_rv = top->v__DOT__lb_padded_2_stencil_update_stream__024reg_0_1__DOT__outReg;
+    assert(top->in_0 == state.self_in_0);
+    uint16_t rv = state.lb_padded_2_stencil_update_stream$reg_0_1;
+    uint16_t eval_rv = top->v__DOT__lb_padded_2_stencil_update_stream__024reg_0_1__DOT__outReg;
 
-    // cout << "state.rv = " << (int) rv << endl;
-    // cout << "top.rv   = " << (int) eval_rv << endl;
+    cout << "state.rv = " << (int) rv << endl;
+    cout << "top.rv   = " << (int) eval_rv << endl;
 
-    // assert(rv == eval_rv);
+    assert(rv == eval_rv);
 
 //     cout << (int) top->v__DOT__lb_grad_xx_2_stencil_update_stream__024mem_2__024mem___05Frdata << endl;
 //     cout << (int) state.lb_grad_xx_2_stencil_update_stream$mem_2$rdata << endl;
@@ -172,8 +272,8 @@ int main(int argc, char** argv) {
 // cout << (int) top->v__DOT__lb_grad_yy_2_stencil_update_stream__024mem_2__024mem___05Frdata << endl;    
 //     cout << (int) state.lb_grad_yy_2_stencil_update_stream$mem_2$rdata$reg0 << endl;
     
-//     cout << "top->out       = " << (int) top->out << endl;
-//     cout << "state.self_out = " << (int) state.self_out << endl;
+    cout << "top->out       = " << (int) top->out << endl;
+    cout << "state.self_out = " << (int) state.self_out << endl;
 
     // Note: Addresses can probably be removed by more advanced logic duplication
 
@@ -184,7 +284,7 @@ int main(int argc, char** argv) {
 
     if (top->clk == 1) {
 
-      //state.self_in_0 = image[i];
+      state.self_in_0 = image[i];
       top->in_0 = image[i];
     }
   }
